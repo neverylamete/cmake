a31b3c8492fcda69fc8c6bac4be981ea4432746a
Anton100 <>
2021-06-15 00:37:59 +0300

    Korner

--- rational_poly/rational_poly.cpp
+++ rational_poly/rational_poly.cpp
@@ -2,12 +2,34 @@
 #include "vector"
 
 
-
 #include "../lib_rational/lib_rational.h"
 //#include "lib_rational.h"
 
+
+double gorner(const std::vector<double> &koef, const double &x)
+{
+    if (koef.empty())
+    {
+        return 0;
+    }
+    if (koef.size() == 1)
+    {
+        return koef[0] * x;
+    }
+
+    double sum = koef[0];
+    for (int i = 1; i < koef.size(); i++)
+    {
+        sum = (sum * x) + koef[i];
+    }
+    return sum;
+}
+
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<double> vec{2, -6, 2, -1};
+    double x = 3;
+    std::cout << gorner(vec, x) << std::endl;
     return 0;
 }
